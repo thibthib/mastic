e5d878924718a8cf649a933c41514fa330192062
Thibaut Dutartre <t.dutartre@gmail.com>
2016-08-28 17:24:08 +0200

    Return asked polyfill

---
+++ packages/mastic-server/server.js
@@ -0,0 +1,32 @@
+const express = require('express')();
+const fs = require('fs');
+
+const getPolyfill = (polyfillName) => {
+	return new Promise((resolve, reject) => {
+		const polyfillPath = require.resolve(`mastic-polyfills/bundles/${polyfillName}.js`);
+		fs.readFile(polyfillPath, 'utf8', (error, contents) => {
+			if (error) {
+				reject();
+			} else {
+				resolve(contents);
+			}
+		});
+	});
+};
+
+express.get('/polyfills.js', (request, response) => {
+	const polyfills = [];
+	if (request.query.with) {
+		request.query.with.split(',').forEach(polyfillName => {
+			polyfills.push(getPolyfill(polyfillName));
+		});
+	}
+	
+	Promise.all(polyfills).then(polyfillsContents => {
+		response.set('Content-Type', 'application/javascript;charset=utf-8');
+		response.set('Access-Control-Allow-Origin', '*');
+		response.send(polyfillsContents.join(''));
+	});
+});
+
+express.listen(4000);
