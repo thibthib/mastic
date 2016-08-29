8a5c9bfa68f7090b26a177b32e4d660e98b30fc9
Thibaut Dutartre <thibaut.dutartre@weekendesk.fr>
2016-08-29 15:37:53 +0200

    Fix scripts loading order

--- packages/mastic-fill/fill.js
+++ packages/mastic-fill/fill.js
@@ -11,20 +11,51 @@ const getPolyfillsURL = (polyfillBundles, { url }) => {
 	return `${url}/polyfills.js?with=${polyfillBundles.join(',')}`;
 };
 
-const loadPolyfills = (url) => {
-	const tag = document.createElement('script');
-	tag.setAttribute('type', 'text/javascript');
-	tag.setAttribute('src', url);
-	document.getElementsByTagName('head')[0].appendChild(tag);
+const getScriptLoader = () => {
+	const loadAsyncScript = (url) => {
+		const tag = document.createElement('script');
+		tag.src = url;
+		tag.async = false;
+		document.head.appendChild(tag);
+	};
+	
+	const pendingScripts = [];
+	const scriptStateChange = () => {
+		while (pendingScripts[0] && pendingScripts[0].readyState === 'loaded') {
+			const scriptToExecute = pendingScripts.shift();
+			scriptToExecute.onreadystatechange = null;
+			document.head.appendChild(scriptToExecute);
+		}
+	};
+	const loadReadyStateScript = (url) => {
+		const tag = document.createElement('script');
+		pendingScripts.push(tag);
+		tag.onreadystatechange = scriptStateChange;
+		tag.src = url;
+	};
+	
+	if ('async' in document.createElement('script')) {
+		return {
+			load: loadAsyncScript
+		};
+	} else {
+		return {
+			load: loadReadyStateScript
+		};
+	}
 };
 
 export default ({ polyfills, url }) => {
+	const scriptLoader = getScriptLoader();
 	const userPolyfills = getUserPolyfills(polyfills);
 	if (userPolyfills.length > 0) {
 		console.log('fetching polyfills : ', userPolyfills.join(', ')); //eslint-disable-line
 		const polyfillURL = getPolyfillsURL(userPolyfills, { url });
-		loadPolyfills(polyfillURL);
+		scriptLoader.load(polyfillURL);
 	} else {
 		console.log('you dont need no polyfill'); //eslint-disable-line
 	}
+	window.polyfilledScripts.forEach(scriptURL => {
+		scriptLoader.load(scriptURL);
+	});
 };
